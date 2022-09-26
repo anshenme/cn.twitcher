a559e1c1bdb5bf27b2fe81ff987f784a507f243c
anshenme <114433012@qq.com>
2022-09-26 17:56:10 +0800

    20220926轮播图更新

--- apps/miniprogram/miniprogram/pages/index/index.js
+++ apps/miniprogram/miniprogram/pages/index/index.js
@@ -1,6 +1,10 @@
+const db = wx.cloud.database()
 Page({
   data: {
     groupId: "",
+    banner: [],
+    num: '',
+    steps: [],
   },
   onLoad: function (params) {
     let groupId = wx.getStorageSync("groupId");
@@ -25,8 +29,28 @@ Page({
           }
         });
     }
+    this.get_banner()
   },
-  onShow: function (params) {
+  //获取轮播图
+  get_banner(){
+    //这个this指向的是Page()方法里面的整个对象，形如Page({this})
+    //赋值给that，that也指向了同样的对象
+    let that = this  
+    db.collection('banner').get({
+      success:function(res){
+         //获取成功
+         //通过setData()方法赋值给data里面的banner数组
+         console.log(res)
+         that.setData({
+            banner:res.data
+         })
+      },
+      fail(er){
+         console.log('获取失败，所以执行fail函数了，也就是打印了我这句话')
+      }
+    })
+  },
+onShow: function (params) {
     let groupId = wx.getStorageSync("groupId");
     if (groupId) {
       this.setData({
